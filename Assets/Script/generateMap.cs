e147e740fca39aeedf2a9bd39fee6aa2db0c31a0
Edgar Maurel <edgar.maurel@epitech.eu>
2023-02-04 16:31:51 +0100

    infinite scroll

--- Assets/Script/generateMap.cs
+++ Assets/Script/generateMap.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class generateMap : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public GameObject parchemin;
+    public Parchemin precedent;
+    public Parchemin parchemin;
     public Camera cam;
     void Start()
     {
@@ -16,9 +17,13 @@ public class generateMap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 position;
-        Quaternion rot;
-        if ()
-            Instantiate(parchemin, position, rot);
+        Vector3 posPrecedent = precedent.transform.position;
+        Vector3 position = new Vector3(posPrecedent.x, posPrecedent.y - 14, posPrecedent.z);
+        Quaternion rot = precedent.transform.rotation;
+        if (precedent.transform.position.y > cam.transform.position.y)
+        {
+            precedent = Instantiate(parchemin, position, rot);
+            
+        }
     }
 }
