34be7779287f93e6e0f0b697fcbacd12517231f6
Edgar Maurel <edgar.maurel@epitech.eu>
2023-02-04 15:24:00 +0100

    player movement

---
+++ Assets/Script/followPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+        if (player.transform.position.y < pos.y - 3f)
+        {
+            this.transform.position = new Vector3(pos.x, pos.y - .03f, pos.z);
+        }
+    }
+}
