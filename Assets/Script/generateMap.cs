e7a07a6dad470c14fdd4111c07139f80b57ee672
Edgar Maurel <edgar.maurel@epitech.eu>
2023-02-04 15:46:48 +0100

    camera follow

---
+++ Assets/Script/generateMap.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject parchemin;
+    public Camera cam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position;
+        Quaternion rot;
+        if ()
+            Instantiate(parchemin, position, rot);
+    }
+}
